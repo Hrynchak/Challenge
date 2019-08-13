e7093d2a18b8509172bac6de76732ab14c6ff2aa
Hrynchak <48262962+Hrynchak@users.noreply.github.com>
2019-08-12 20:26:41 -0500

    Update mysql.js

--- models/mysql.js
+++ models/mysql.js
@@ -4,10 +4,10 @@ const
   { MYSQL_HOST, MYSQL_USER, MYSQL_PASSWORD, MYSQL_DATABASE } = process.env
 
 const db = mysql.createConnection({
-  host: 'm7nj9dclezfq7ax1.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
-  user: 'ub1b4yoncojtvt2w',
-  password: 'l6hqwusuazvk9fc2',
-  database: 'kw0ddgnkk8d3b4s3',
+  host: 'c584md9egjnm02sk.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+  user: 'ghzocq16mvrw0ml0',
+  password: 'at2o0dvtk73er2vn',
+  database: 'a15sfp68k8d1zqnp',
   charset: "utf8mb4"
 })
 
